a42d97f64395e21d6e08c10e7aa995d3e6c9faf1
Jamie Hughes <jamie.hughes@ukaea.uk>
2024-10-08 13:20:10 +0100

    Docstring improvements

--- inflammation/models.py
+++ inflammation/models.py
@@ -41,16 +41,32 @@ def load_json(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    Input: Data - 2D data array with inflammation data for each day.
+
+    Return: Funtion returns the mean of the data array for each day.
+
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    Input: Data - 2D data array with inflammation data for each day.
+
+    Return: Funtion returns the max of the data array for each day.
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    Input: Data - 2D data array with inflammation data for each day.
+
+    Return: Funtion returns the min of the data array for each day.
+    """
     return np.min(data, axis=0)
 
